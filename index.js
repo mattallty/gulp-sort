270c1bba604fd524e4475855528af3719435c016
Matt <matt@allty.com>
2014-07-18 18:00:21 +0200

    maj

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 var through = require('through')
 var path = require('path')
-require("natural-compare-lite")
 
-function gulpSort(opts, sortMethod) {
+function gulpSort(sortMethod) {
 
   var files = [],
     sortMethod = sortMethod ||
       /**
-       * naturalCmp function taken from Underscore.string
+       * Adapted from String.naturalCompare
+       * by Lauri Rooden (https://github.com/litejs/natural-compare-lite)
        */
-      function (p1, p2) {
-        var str1 = path.basename(p1.path).replace(/[-_]/g, '')
-        var str2 = path.basename(p2.path).replace(/[-_]/g, '')
-        return String.naturalCompare(str1, str2)
+        function (p1, p2) {
+        var a = path.basename(p1.path, path.extname(p1.path)).replace(/[^a-z0-9]/g, String.fromCharCode(91))
+        var b = path.basename(p2.path, path.extname(p2.path)).replace(/[^a-z0-9]/g, String.fromCharCode(91))
+
+        if (a != b) for (var i, ca, cb = 1, ia = 0, ib = 0; cb;) {
+          ca = a.charCodeAt(ia++) || 0
+          cb = b.charCodeAt(ib++) || 0
+
+          if (ca < 58 && ca > 47 && cb < 58 && cb > 47) {
+            for (i = ia; ca = a.charCodeAt(ia), ca < 58 && ca > 47; ia++);
+            ca = (a.slice(i - 1, ia) | 0) + 1
+            for (i = ib; cb = b.charCodeAt(ib), cb < 58 && cb > 47; ib++);
+            cb = (b.slice(i - 1, ib) | 0) + 1
+          }
+
+          if (ca != cb) return (ca < cb) ? -1 : 1
+        }
+        return 0
       }
 
 
@@ -22,9 +36,6 @@ function gulpSort(opts, sortMethod) {
 
   var onEnd = function onEnd() {
     var that = this
-
-    //localeCompare
-
     files.sort(sortMethod).forEach(function (file) {
       that.emit('data', file)
     })
